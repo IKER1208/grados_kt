630854610803024ac3193c294c9d6e7d08069fb1
IKER1208 <iker34267@gmail.com>
2025-02-14 07:25:21 -0600

    cambios leves

--- app/src/main/java/com/example/grados_kt/MainActivity.kt
+++ app/src/main/java/com/example/grados_kt/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.tuapp.conversiongrados
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.grados_kt.databinding.ActivityMainBinding
-
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
@@ -12,17 +13,35 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //spinner
+        val opciones = listOf("Celsius", "Kelvin", "Fahrenheit")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, opciones)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spinnerTipoGrado.adapter = adapter
+
         binding.btnConvertir.setOnClickListener {
             val valor = binding.etValor.text.toString().toDoubleOrNull()
+            val seleccion = binding.spinnerTipoGrado.selectedItem.toString()
 
             if (valor != null) {
-                val grado = Celsius(valor)
-                val resultadoF = grado.convertir(Fahrenheit(0.0))
-                val resultadoK = grado.convertir(Kelvin(0.0))
-
-                binding.tvResultado.text = "Fahrenheit: $resultadoF\nKelvin: $resultadoK"
+                val resultado = when (seleccion) {
+                    "Celsius" -> {
+                        val celsius = Celsius(valor)
+                        "Fahrenheit: ${celsius.convertir(Fahrenheit(0.0))} \nKelvin: ${celsius.convertir(Kelvin(0.0))}"
+                    }
+                    "Kelvin" -> {
+                        val kelvin = Kelvin(valor)
+                        "Celsius: ${kelvin.convertir(Celsius(0.0))} \nFahrenheit: ${kelvin.convertir(Fahrenheit(0.0))}"
+                    }
+                    "Fahrenheit" -> {
+                        val fahrenheit = Fahrenheit(valor)
+                        "Celsius: ${fahrenheit.convertir(Celsius(0.0))} \nKelvin: ${fahrenheit.convertir(Kelvin(0.0))}"
+                    }
+                    else -> "Selección inválida"
+                }
+                binding.tvResultado.text = resultado
             } else {
-                binding.tvResultado.text = "Ingresa un valor válido"
+                Toast.makeText(this, "Ingrese un valor válido", Toast.LENGTH_SHORT).show()
             }
         }
     }
